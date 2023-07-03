9dcf450ea106b57182a6656da839cc6d4f111964
AlvinTuan <letuan7923@gmail.com>
2023-07-03 10:40:53 +0700

    home-ver1

--- src/components/exploreDAO/ExploreDAO.tsx
+++ src/components/exploreDAO/ExploreDAO.tsx
@@ -6,13 +6,13 @@ import ExploreDaoBtn from "./ExploreDaoBtn"
 import ExploreDAOimg from "./ExploreDAOimg"
 
 const ExploreDAO = () => {
-
+    const [clickShow, setClickShow] = useState(true)
     const [exploreList, setExploreList] = useState<ExploreItem[]>([])
     useEffect(() => {
         const fetchData = () => {
             setExploreList(exploreDAO);
         }
-        const delay = 50000
+        const delay = 3000
         const timerID = setTimeout(fetchData, delay)
         return () => clearTimeout(timerID)
     }, [])
@@ -22,15 +22,15 @@ const ExploreDAO = () => {
                 <p className='explore__title'>
                     Explore DAOs
                 </p>
-                {exploreList.length === 0 ? <div className="loading"></div> : <div className="explore__show--item">
+                {exploreList.length === 0 ? <div className="loading--item"></div> : <div className="explore__show--item">
                     {exploreList.map(item => (
                         item.avatar.endsWith("jpeg" || "jpg" || "png") ? <ExploreDAOimg item={item} key={item.id}></ExploreDAOimg> : <ExploreDaoBtn item={item} key={item.id}></ExploreDaoBtn>
                     ))}
-                    <button className="show_btn">
+                    <button className="show_btn" onClick={() => setClickShow(false)}>
                         <span>Show more</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="chevron--down">
+                        {clickShow ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="chevron--down">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                        </svg>
+                        </svg> : <div className="loading--show"></div>}
                     </button>
                 </div>}
 
